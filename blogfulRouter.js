6e12284d5ad6cf135347325f6b87d5d4b7bdbdcf
David Acosta <acosta816@gmail.com>
2020-05-30 18:10:33 -0700

    finished routing blogful express api w/CRUD operations

---
+++ blogfulRouter.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const {BlogPosts} = require('./models');
+
+//adding some default data to work with 
+
+BlogPosts.create("Ice Climbing", "ice climning is pretty fun, you really should give it a try", "Lara Croft");
+BlogPosts.create("How to make orchata", ["step1: boil rice","step2: strain milk", "step3: add cinnamon, ice and dates for sweetness"], "Nathaniel Acosta");
+
+router.get('/', (req,res) => {
+    res.json(BlogPosts.get());
+});
+
+router.post('/', jsonParser, (req,res) => {
+    if(!req.body.title && !req.body.content && !req.body.author){
+        return res.status(400).send("Must include, title, content, and author");
+    }
+    const {title, content, author} = req.body;
+    const blogPost = BlogPosts.create(title, content, author);
+    res.status(201).json(blogPost);
+})
+
+router.put('/:id', jsonParser, (req,res) => {
+    if(req.params.id !== req.body.id){
+        res.status(400).send("ERROR: either your id doesn't exist or ir doesn't match with the one in the url");
+    }
+    BlogPosts.update(req.body);
+    res.status(204).end();
+})
+
+router.delete('/:id', (req,res) => {
+    if(!BlogPosts.posts.find(post=> post.id === req.params.id ) ){
+        return res.status(400).send("ERROR: id provided in url does not match id of any post in BlogPosts.posts");
+    }
+    console.log("deleting: ", BlogPosts.posts.find(post=> post.id === req.params.id));
+    BlogPosts.delete(req.params.id);
+    res.status(204).end();
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("hello");
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+const blogfulRouter = require('./blogfulRouter');
+
+//log the http layer
+app.use(morgan('common'));
+
+app.use(express.static('public'));
+
+app.use('/blogful', blogfulRouter);
+
+
+app.listen(process.env.PORT || 8080, ()=> {
+    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
